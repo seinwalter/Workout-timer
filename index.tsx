3de266b5603c3e72164ff7add6ec5dc64b14a15b
seinwalter <122806497+seinwalter@users.noreply.github.com>
2024-12-30 22:31:06 -0600

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import WorkoutTimer from './WorkoutTimer';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <WorkoutTimer />
+  </React.StrictMode>
+);
